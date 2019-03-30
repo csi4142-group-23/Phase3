5dbb9d7893830e993193d25433e61f0a7473ae02
Jasper Yiu <yiujasper@gmail.com>
2019-03-30 14:07:42 -0400

    trained score: approx 67-74%

--- Phase3.py
+++ Phase3.py
@@ -7,6 +7,26 @@ import pandas as pd
 import math #for nan check
 from datetime import *
 
+
+# Classifiers
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.dummy import DummyClassifier
+
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
+
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler
+
 def preprocess(df):
     df = df.drop(['COLLISION_ID','LOCATION','X','Y'], 1)
 
@@ -48,7 +68,55 @@ def preprocess(df):
         df.at[index, 'IMPACT_TYPE'] = row['IMPACT_TYPE'][:2]
 
 
-    print(df.head(5))
+    df['TARGET'] = df.iloc[:,6] # TARGET = SURFACE_CONDITION
+    df = df[['DATE','TIME','ENVIRONMENT','LIGHT','TRAFFIC_CONTROL','TRAFFIC_CONTROL_CONDITION', 'COLLISION_CLASSIFICATION', 'IMPACT_TYPE', 'TARGET']]
+
+    # remove rows with nans - results in removal of 7390 rows (from 14843 to 7453)
+    df = df.loc[df.TRAFFIC_CONTROL_CONDITION.apply(type) != float]
+    df = df.loc[df.TRAFFIC_CONTROL.apply(type) != float]
+
+    df = df.drop(df[df.TARGET == '01'].index)
+
+    return df
+
+
+def train(df):
+
+    print(df.head(3))
+    
+
+    features = ['DATE','TIME','ENVIRONMENT','LIGHT','TRAFFIC_CONTROL','TRAFFIC_CONTROL_CONDITION', 'COLLISION_CLASSIFICATION', 'IMPACT_TYPE']
+    target = 'TARGET'
+
+    # ---------------------------------------- train model ------------------------------------
+    X_train, X_test, y_train, y_test = train_test_split(df[features], df[target], test_size=0.33)  # for personal testing (will not be used in final program)
+
+    scaler = StandardScaler()
+    X_train = scaler.fit(X_train).transform(X_train)
+
+    #X_test = X_test.fillna(X_train.mean())
+
+    classifier = OneVsRestClassifier(LinearSVC())
+    classifier.fit(X_train, y_train)
+
+
+
+    # ---------------------------------------- results ------------------------------------
+    X_test = scaler.fit(X_test).transform(X_test)
+    predictions = classifier.predict(X_test)
+    print(predictions)
+
+    print(classifier.score(X_test, y_test))
+
+
+    # ---------------------------------------- Graph of prediction ------------------------------------
+    #plt.scatter(y_test, predictions)
+    #plt.xlabel('True Values')
+    #plt.ylabel('Predictions')
+    #plt.show()
+    
+
+
 
 ''' ----------------------------------------------------------------------------------------
     ------------------------------------- MAIN PROGRAM ------------------------------------- 
@@ -59,7 +127,12 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 pd.set_option('display.expand_frame_repr', False) # --- display whole dataframe
 
+np.set_printoptions(threshold=np.inf)
 
 df = pd.read_csv("2014collisionsfinal.csv")
-preprocess(df)
+df = preprocess(df)
 
+# drop rows with missing values
+#df = df.dropna(inplace=True)
+
+train(df)
